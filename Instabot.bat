cf02249632961c8f1f56b160d0ea77d8f75d1c20
Yunus Emre <yedhrab@gmail.com>
2019-05-30 19:51:30 +0300

    Hatalar giderildi

--- Instabot.bat
+++ Instabot.bat
@@ -1,22 +1,27 @@
 cls
 @ECHO OFF
-cd utils
-IF EXIST "downloaded.dat" (
-    IF EXIST "installed.dat" (
-        cd ..
-        start "" InstagramBot.jar
+IF EXIST "utils/downloaded.dat" (
+    IF EXIST "utils/installed.dat" (
+        IF EXIST "utils/module.dat" (
+            echo "Setup complated, just runing the application"
+            start "" InstagramBot.jar
+        ) ELSE (
+            cd insta
+            "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe" setup.py install
+            cd ..
+
+            cd utils
+            echo "Java 8u211 & Python 3.7.3" > module.dat
+            
+            cd ..
+            Instabot.bat
+        )
     ) ELSE (
+        cd utils
         start /w jre-8u211-windows-i586.exe /s INSTALLDIR=%userprofile%\AppData\Local\Programs\java\jre1.8.2
 
-        python-3.7.3.exe InstallAllUsers=0 Include_launcher=0 Include_test=0 SimpleInstall=1 SimpleInstallDescription="Just for Instobot 🤖"
+        python-3.7.3.exe InstallAllUsers=0 Include_launcher=0 Include_test=0 SimpleInstall=1 SimpleInstallDescription="Just for Instobot"
 
-        set JRE_HOME=%userprofile%\AppData\Local\Programs\java\jre1.8.2
-        set PATH=%userprofile%\AppData\Local\Programs\Python\Python37
-
-        cd insta
-        python setup.py install
-        cd ..
-        
         echo "Java 8u211 & Python 3.7.3" > installed.dat
         echo "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe" > instalib\info.dat
         
@@ -24,6 +29,7 @@ IF EXIST "downloaded.dat" (
         Instabot.bat
     )
 ) ELSE (
+    cd utils
     (wget.exe -O "python-3.7.3.exe" "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe" && wget.exe -O "jre-8u211-windows-i586.exe" "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=238727_478a62b7d4e34b78b671c754eaaf38ab") || echo "Error while installation" && pause && exit
 
     REM del /s / q .wget-hsts
