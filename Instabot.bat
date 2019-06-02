b525357fd52ef18ccd6eb6a17e493849705994fc
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-06-02 18:46:56 +0300

    Python yüklü ise yapılan değişiklik geri alındı

--- Instabot.bat
+++ Instabot.bat
@@ -8,9 +8,9 @@ IF EXIST "utils/downloaded.dat" (
         ) ELSE (
             copy utils/fcntl.py "%userprofile%\AppData\Local\Programs\Python\Python37"
             
-            python -m pip install --upgrade pip || "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe" -m pip install --upgrade pip
+            "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe" -m pip install --upgrade pip
 
-            pip install instapy || "%userprofile%\AppData\Local\Programs\Python\Python37\Scripts\pip.exe" install instapy
+            "%userprofile%\AppData\Local\Programs\Python\Python37\Scripts\pip.exe" install instapy
             
             echo "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe" > instalib\info.dat
 
